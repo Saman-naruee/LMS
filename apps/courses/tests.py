e80311f835f605de8f16ebd9f99f98d4c470e973
Saman-naruee <samannaruee@gmail.com>
2025-12-04 20:00:53 +0330

    feat: Implement asynchronous course creation
    - Preparing Celery for background course creation tasks.
    - Resolved NoReverseMatch and test discovery issues.
    - Updated course creation tests with all required fields.
    - Added pps/courses/urls.py and serializers.py.

--- apps/courses/tests.py
+++ apps/courses/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from apps.courses.models import Course
+from apps.accounts.models import User # Import the User model
+from django.urls import reverse
+from datetime import datetime
+from django.utils import timezone
 
-# Create your tests here.
+
+class CourseCreateTest(TestCase):
+    def setUp(self):
+        self.url = reverse('create-course')
+        # Provide a phone_number when creating the user
+        self.user = User.objects.create_user(
+            username='testuser', 
+            email='test@example.com', 
+            password='password',
+            phone_number=1234567890
+        )
+        print(self.user.id)
+    def test_create_course_returns_201(self):
+        start_date = timezone.make_aware(datetime(2025, 1, 1, 9, 0, 0))
+        end_date = timezone.make_aware(datetime(2025, 1, 31, 17, 0, 0))
+
+        payload = {
+            "title": "Test Course",
+            "teacher": self.user.id,
+            "start_date": start_date.isoformat(),
+            "end_date": end_date.isoformat(),
+        }
+        response = self.client.post(self.url, data=payload, content_type="application/json")
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(Course.objects.filter(title="Test Course").exists())
