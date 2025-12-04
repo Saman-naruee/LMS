f7444c62c5582e0299824d23b304976d1b6c76a9
Saman-naruee <samannaruee@gmail.com>
2025-12-04 17:08:05 +0330

    Update settings as postgres.

--- lms_live/settings.py
+++ lms_live/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+from custom_logger import custom_logger as print
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,13 +75,32 @@ WSGI_APPLICATION = 'lms_live.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+SQLITE3 = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+POSTGRES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", cast=int, default=5432),
+    }
+}
+
+try:
+    PREFFRED = config('preffred_database', cast=str)
+    PREFFRED = POSTGRES if PREFFRED == 'POSTGRES' else SQLITE3
+except:
+    PREFFRED = SQLITE3
+
+# Use the selected database configuration directly; it already contains the required 'default' key.
+DATABASES = PREFFRED
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
