daec1485503c2aa98843813471344f5700188116
Saman-naruee <samannaruee@gmail.com>
2025-12-08 19:38:33 +0330

    Implement a proper base for account view tests.

--- apps/accounts/tests/view_tests/fetch_user_data_tests.py
+++ apps/accounts/tests/view_tests/fetch_user_data_tests.py
@@ -2,6 +2,8 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from apps.accounts.models import User
 from apps.accounts.tests.view_tests.base_view_tests import BaseViewTestClass
+from custom_logger import custom_logger as print
+
 
 class FetchUserInfos(BaseViewTestClass):
     def setUp(self):
@@ -10,4 +12,5 @@ class FetchUserInfos(BaseViewTestClass):
 
     def test_fetch_users_data(self):
         response = self.client.get(self.users_url)
+        print(response)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
